9117259ff40de0bc60eb30222b96c9b6206505bc
dani816 <danijelhrvatin29@gmail.com>
2024-09-06 15:26:25 +0200

    first commit

---
+++ src/main/java/com/danijel/bank_app_leapwise/BankAppLeapwiseApplication.java
@@ -0,0 +1,14 @@
+package com.danijel.bank_app_leapwise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankAppLeapwiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankAppLeapwiseApplication.class, args);
+		System.out.println("I'm working");
+	}
+
+}
---
+++ src/test/java/com/danijel/bank_app_leapwise/BankAppLeapwiseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danijel.bank_app_leapwise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankAppLeapwiseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
