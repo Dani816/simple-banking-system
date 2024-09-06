a94ec9607f8c6f9e63aa76992edbc5cfeaf96584
dani816 <danijelhrvatin29@gmail.com>
2024-09-06 17:14:11 +0200

    Added controller, service, repository classes

---
+++ src/main/java/com/danijel/bank_app_leapwise/InitData.java
@@ -0,0 +1,18 @@
+package com.danijel.bank_app_leapwise;
+
+import jakarta.annotation.PostConstruct;
+
+public class InitData {
+
+    public InitData (
+
+    ){
+
+    }
+
+    @PostConstruct
+    public void init() {
+
+    }
+
+}
---
+++ src/main/java/com/danijel/bank_app_leapwise/repository/AccountRepository.java
@@ -0,0 +1,11 @@
+package com.danijel.bank_app_leapwise.repository;
+
+import com.danijel.bank_app_leapwise.model.entity.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AccountRepository extends JpaRepository<Account, Long> {
+
+
+}
