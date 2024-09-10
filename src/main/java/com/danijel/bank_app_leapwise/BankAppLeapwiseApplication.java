b625291297b851bc90d90d8a957c38987aaf00b1
dani816 <danijelhrvatin29@gmail.com>
2024-09-10 18:25:11 +0200

    Added model mapper

--- src/main/java/com/danijel/bank_app_leapwise/BankAppLeapwiseApplication.java
+++ src/main/java/com/danijel/bank_app_leapwise/BankAppLeapwiseApplication.java
@@ -1,7 +1,11 @@
 package com.danijel.bank_app_leapwise;
 
+import org.modelmapper.Conditions;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BankAppLeapwiseApplication {
@@ -11,4 +15,11 @@ public class BankAppLeapwiseApplication {
 		System.out.println("I'm working");
 	}
 
+	@Bean
+	public ModelMapper modelMapper() {
+		ModelMapper modelMapper = new ModelMapper();
+		modelMapper.getConfiguration().setPropertyCondition(Conditions.isNotNull())
+				.setMatchingStrategy(MatchingStrategies.STRICT);
+		return modelMapper;
+	}
 }
